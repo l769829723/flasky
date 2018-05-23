b3b2f8622c51e2067dd3f9c1f66964d9a2b5307e
李 春阳 <chunyang.li@cloudrabbit.cn>
2018-05-23 17:33:45 +0800

    reder template

--- hello.py
+++ hello.py
@@ -1,22 +1,18 @@
 from flask import Flask
-from flask import request
-from flask import make_response
-from flask import redirect
+from flask import render_template
 from flask_script import Manager
 
 app = Flask(__name__)
-manager = Manager(app)
+app.debug = True
+manager = Manager(app) 
 
 @app.route('/')
 def index():
-  # response = make_response('<h1>This document carries a cookie!</h1>')
-  # user_agent = request.headers.get('User-Agent')
-  # response.set_cookie('answer', '42')
-  return redirect('https://www.baidu.com')
+  return render_template('index.html')
 
 @app.route('/user/<name>/')
 def user(name):
-  return '<h1>Hello, {}!</h1>'.format(name)
+  return render_template('user.html', name=name)
 
 if __name__ == '__main__':
   manager.run()
