086ca99cb9bd76c1aefcf4e2c3a790adc4e51dec
李 春阳 <chunyang.li@cloudrabbit.cn>
2018-05-24 15:36:22 +0800

    flask flash message;

--- hello.py
+++ hello.py
@@ -24,6 +24,9 @@ class NameForm(Form):
 def index():
   form = NameForm()
   if form.validate_on_submit():
+    old_name = session.get('name')
+    if old_name is not None and old_name != form.name.data:
+      flash('Looks like you have changed your name!')
     session['name'] = form.name.data
     return redirect(url_for('index'))
   return render_template('index.html', form=form, name=session.get('name'))
