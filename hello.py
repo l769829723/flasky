40dd335a56d36bc781e6d07feb9285efa1cd095b
李 春阳 <chunyang.li@cloudrabbit.cn>
2018-05-24 14:13:04 +0800

    template and extends

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ from flask_script import Manager
 
 app = Flask(__name__)
 app.debug = True
+app.config['SECRET_KEY'] = 'vLIiT3Qcht68WF8NakLeeGOGLuGgluuGdEVK6psOXsDdO3u3'
 manager = Manager(app) 
 
 @app.route('/')
@@ -13,6 +14,14 @@ def index():
 @app.route('/user/<name>/')
 def user(name):
   return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+  return render_template('500.html'), 500
 
 if __name__ == '__main__':
   manager.run()
